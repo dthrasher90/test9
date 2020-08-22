149915a038bb701d677086f127a3bebb4333eae4
dthrasher90 <dthrasher90@GMAIL.COM>
2020-08-22 09:33:03 -0700

    changes to title

---
+++ src/nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Navbar } from 'react-bootstrap';
+import './Nav.css';
+
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        {these code will be rendered into the DOM}
+      </div>
+    )
+  }
+}
